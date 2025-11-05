2d801de8332b5dfac99457b1768b2a165b3380b1
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-11-04 22:46:57 -0300

    Implementando estrutura inicial para templates_app, incluindo models e migrations

---
+++ templates_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
+
+class Template(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    file = models.FileField(
+        upload_to="templates/",
+        validators=[FileExtensionValidator(allowed_extensions=["docx", "doc", "txt"])],
+        verbose_name="Nome do template"
+    )
+    placeholders = models.JSONField(
+        default=list,
+        verbose_name="Lista de placeholders do template"
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="Data de criação"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Data de atualização"
+    )
+
+    class Meta:
+        verbose_name = "Template"
+        verbose_name_plural = "Templates"
+        ordering = ["-created_at"]
+
+
+    def __str__(self):
+        return self.name
