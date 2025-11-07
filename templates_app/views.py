0dd720675710de21753dafe3cfea06b0ac0aed1d
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-11-06 21:31:57 -0300

    fix: Refatora método create em TemplateViewSet para melhorar handling para a extração de placeholders

--- templates_app/views.py
+++ templates_app/views.py
@@ -3,13 +3,12 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.exceptions import APIException
-from django.db import transaction
 
 from templates_app.models import Template
 from templates_app.serializers import TemplateSerializer
 from templates_app.utils.placeholder_extractor import PlaceholderExtractor
-from core.utils.exceptions import ValidationError  # se existir no seu projeto
-from core.utils.formatters import format_serializer_error  # idem
+from core.utils.exceptions import ValidationError
+from core.utils.formatters import format_serializer_error
 
 
 class TemplateViewSet(viewsets.ModelViewSet):
@@ -20,10 +19,27 @@ class TemplateViewSet(viewsets.ModelViewSet):
 
     def create(self, request: Request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        template = serializer.save()
-        return Response(TemplateSerializer(template).data, status=status.HTTP_201_CREATED)
 
+        if not serializer.is_valid():
+            error_message = format_serializer_error(serializer.errors)
+            raise ValidationError(detail=error_message)
+
+        # Salva o template
+        template = serializer.save()
+
+        # Extração de placeholders do arquivo
+        try:
+            extractor = PlaceholderExtractor(template.file.path)
+            template.placeholders = extractor.extract()
+            template.save()
+        except Exception as e:
+            # Não quebra o fluxo, apenas loga o erro
+            print(f"[ERRO] Falha ao extrair placeholders: {e}")
+
+        return Response(
+            TemplateSerializer(template).data,
+            status=status.HTTP_201_CREATED
+        )
 
     @decorators.action(detail=True, methods=['get'])
     def placeholders(self, request: Request, pk=None):
